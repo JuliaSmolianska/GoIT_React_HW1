07859cc5ef7055a5566cf7efe20086771b9d2d91
JuliaSmolianska <i.julia.i1990@gmail.com>
2023-08-21 09:42:07 +0300

    fixed Statistics component

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -11,7 +11,7 @@ const Statistics = ({ stats, title }) => {
   ];
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.stat_list}>
         {stats.map((item, index) => (
           <li
