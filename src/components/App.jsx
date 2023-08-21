37550f73b4154db4f5ee34ddc5d49957e01e7b20
JuliaSmolianska <i.julia.i1990@gmail.com>
2023-08-21 09:35:01 +0300

    added props type

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,10 +10,28 @@ import TransactionHistory from './Transaction/TransactionHistory';
 export const App = () => {
   return (
     <div>
-      <Profile user={user} />
-      <Statistics stats={data} />
-      <FriendsList friends={friends} />
-      <TransactionHistory items={transactions} />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+        user={user}
+      />
+      <Statistics stats={data} title="Upload stats" />
+      <FriendsList
+        avatar={friends.avatar}
+        name={friends.name}
+        isOnline={friends.isOnline}
+        friends={friends}
+      />
+      <TransactionHistory
+        id={transactions.id}
+        type={transactions.type}
+        amount={transactions.amount}
+        currency={transactions.currency}
+        items={transactions}
+      />
     </div>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,17 @@
 import StatisticsCard from './StatisticsCard';
 import css from './Statistics.module.css';
 
-const Statistics = ({ stats }) => {
-  const colorBackground = ['lightblue', 'purple', 'orange', 'red', 'yellowgreen'];
+const Statistics = ({ stats, title }) => {
+  const colorBackground = [
+    'lightblue',
+    'purple',
+    'orange',
+    'red',
+    'yellowgreen',
+  ];
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>Upload stats</h2>
+      <h2 className={css.title}>{title}</h2>
       <ul className={css.stat_list}>
         {stats.map((item, index) => (
           <li
