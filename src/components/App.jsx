5357da5eb093f4ea8a9502946ac8db24a63d6396
JuliaSmolianska <i.julia.i1990@gmail.com>
2023-08-21 11:26:35 +0300

    added PropTypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import transactions from '../data/transactions.json';
 import Statistics from './Statistics/Statistics';
 import FriendsList from './FriendsList/FriendsList';
 import TransactionHistory from './Transaction/TransactionHistory';
+import PropTypes from 'prop-types';
 
 export const App = () => {
   return (
@@ -34,4 +35,44 @@ export const App = () => {
       />
     </div>
   );
+};
+
+Profile.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+Statistics.propTypes = {
+  stats: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+FriendsList.propTypes = {
+  friends: PropTypes.shape({
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+TransactionHistory.propTypes = {
+  payment: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    amount: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+  }).isRequired,
 };
